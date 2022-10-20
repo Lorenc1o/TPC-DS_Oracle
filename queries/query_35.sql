c251615b2c4519f15e5e55376ce28746d84c62b3
Ra1-2018 <nikola.iv.99@gmail.com>
2022-10-20 10:41:09 +0200

    Update query_35.sql

--- queries/query_35.sql
+++ queries/query_35.sql
@@ -13,19 +13,19 @@ select * from (select
   cd_marital_status,
   cd_dep_count,
   count(*) cnt1,
-  max(cd_dep_count) max1,
-  stddev_samp(cd_dep_count) sum1,
-  stddev_samp(cd_dep_count) max2,
+  avg(cd_dep_count),
+  max(cd_dep_count),
+  sum(cd_dep_count),
   cd_dep_employed_count,
   count(*) cnt2,
-  max(cd_dep_employed_count) max3,
-  stddev_samp(cd_dep_employed_count) sum2,
-  stddev_samp(cd_dep_employed_count) max4,
+  avg(cd_dep_employed_count),
+  max(cd_dep_employed_count),
+  sum(cd_dep_employed_count),
   cd_dep_college_count,
   count(*) cnt3,
-  max(cd_dep_college_count) max5,
-  stddev_samp(cd_dep_college_count) sum3,
-  stddev_samp(cd_dep_college_count) max6
+  avg(cd_dep_college_count),
+  max(cd_dep_college_count),
+  sum(cd_dep_college_count)
  from
   customer c,customer_address ca,customer_demographics
  where
@@ -35,19 +35,19 @@ select * from (select
           from store_sales,date_dim
           where c.c_customer_sk = ss_customer_sk and
                 ss_sold_date_sk = d_date_sk and
-                d_year = 2000 and
+                d_year = 1999 and
                 d_qoy < 4) and
    (exists (select *
             from web_sales,date_dim
             where c.c_customer_sk = ws_bill_customer_sk and
                   ws_sold_date_sk = d_date_sk and
-                  d_year = 2000 and
+                  d_year = 1999 and
                   d_qoy < 4) or 
     exists (select * 
             from catalog_sales,date_dim
             where c.c_customer_sk = cs_ship_customer_sk and
                   cs_sold_date_sk = d_date_sk and
-                  d_year = 2000 and
+                  d_year = 1999 and
                   d_qoy < 4))
  group by ca_state,
           cd_gender,
