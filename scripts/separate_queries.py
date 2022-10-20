26108e3024afec6efe892a890b1d83598f6c7c5a
SYusupov <36813444+SYusupov@users.noreply.github.com>
2022-10-21 00:07:10 +0200

    Updating based on generate_queries.py

--- scripts/separate_queries.py
+++ scripts/separate_queries.py
@@ -23,18 +23,12 @@ with open(read_path, "r") as f:
     lines_to_write = []
     for line in lines_to_read:
         if "-- start query " in line:
-            lines_to_write.append(line)
-            query_n = re.search(r'\d+', line).group()
-            query_filename = "query_"+str(query_n)+".sql"
+            query_n = re.search(r'query\d+', line).group()
+            query_filename = query_n+".sql"
             query_path = os.path.join(args.directory, query_filename)
 
             # timing and outputing the result
             lines_to_write.append(
-                "WHENEVER SQLERROR EXIT 1\n"
-                "SET LINES 32000\n"
-                "SET TERMOUT OFF ECHO OFF NEWP 0 SPA 0 PAGES 0 FEED OFF HEAD OFF TRIMS ON TAB OFF\n"
-                "SET SERVEROUTPUT OFF\n"
-                "\n"
                 f"spool &1\n"
                 "timing start t\n\n"
             )
@@ -45,7 +39,6 @@ with open(read_path, "r") as f:
                 "spool off\n"
                 "exit\n"
             )
-            lines_to_write.append(line)
             with open(query_path, "w") as query_f:
                 query_f.writelines(lines_to_write)
                 lines_to_write = []
